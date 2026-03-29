e423f32d56bc7af348e192e9be7e423133b5760a
thevedantsharma <151762644+thevedantsharma@users.noreply.github.com>
2026-03-29 23:16:02 +0530

    Fix Express v5 CORS issue

--- server/server.js
+++ server/server.js
@@ -26,6 +26,12 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://attendance-tracking-system-taupe.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 
 // Security Middleware
