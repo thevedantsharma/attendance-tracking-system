4e0517b885f50370099a9df45db37e19baf43915
thevedantsharma <151762644+thevedantsharma@users.noreply.github.com>
2026-03-29 22:16:27 +0530

    Fixed CORS for production

--- server/server.js
+++ server/server.js
@@ -22,9 +22,11 @@ const PORT = process.env.PORT || 5000;
 
 // Security Middleware
 app.use(helmet());
+const cors = require('cors');
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
+  origin: 'https://attendance-tracking-system-taupe.vercel.app',
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
